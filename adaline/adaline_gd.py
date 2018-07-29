cbb95dd3aa870a4e8d1972aced323ea412289965
Jarred Parr <jparr721@gmail.com>
2018-07-29 13:58:27 -0400

    editing adaptive linear neuron model

---
+++ adaline/adaline_gd.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class AdalineGD(object):
+    """Adaptive linear neuron classifier"""
+
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """Fit our training data"""
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
--- perceptron/flowers.py
+++ perceptron/flowers.py
@@ -60,13 +60,13 @@ and fit it to a graph to show the number of iterations
 """
 ppn = Perceptron(eta=0.1, n_iter=10)
 ppn.fit(X, y)
-# plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-# plt.xlabel('Epochs (iterations)')
-# plt.ylabel('Number of updates')
-# plt.show()
-
-plot_decision_regions(X, y, classifier=ppn)
-plt.xlabel('sepal length [cm]')
-plt.ylabel('petal length [cm]')
-plt.legend(loc='upper left')
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs (iterations)')
+plt.ylabel('Number of updates')
 plt.show()
+
+# plot_decision_regions(X, y, classifier=ppn)
+# plt.xlabel('sepal length [cm]')
+# plt.ylabel('petal length [cm]')
+# plt.legend(loc='upper left')
+# plt.show()
